1b25427d545b181235038877b88c30c4f04150fc
abraham Sosa <abraham.sosa.vel@gmail.com>
2023-01-15 19:49:30 -0400

    resolve problmes

--- src/config.js
+++ src/config.js
@@ -7,3 +7,5 @@ export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "juniorsosa";
 
 export const DB_NAME = process.env.DB_NAME || "sepdaviDB";
+
+export const DB_PORT = process.env.DB_PORT || "5432";
--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import Sequelize from "sequelize";
-import { DB_HOST, DB_PASSWORD, DB_NAME } from "./config";
+import { DB_HOST, DB_PASSWORD, DB_NAME, DB_PORT } from "./config";
 const sequelize = new Sequelize(DB_NAME, "postgres", DB_PASSWORD, {
   host: DB_HOST,
+  port: DB_PORT,
   dialect: "postgres",
 });
 
